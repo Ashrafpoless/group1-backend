a9cd9b75e048e44a128790ca36673ffd2a66cfb3
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-05-05 22:53:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5050;
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // Allow this specific origin
+    origin: 'https://group1-frontend-six.vercel.app', // Allow this specific origin
     methods: 'GET,POST,PUT,DELETE',  // Specify allowed HTTP methods
     credentials: true                // Enable sending cookies with requests
 }));
